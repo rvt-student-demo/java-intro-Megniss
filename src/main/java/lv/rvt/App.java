c1ed0178a7bbd2fa6a19e2670374500ddb25435c
Megniss <166013473+Megniss@users.noreply.github.com>
2024-09-27 08:15:45 +0000

    Number of Numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,10 +2,25 @@ package lv.rvt;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        // Make your first Hello World app!
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Give the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        if (num2 != 0) {
+            System.out.println(num1 + " / " + num2 + " = " + ((double) num1 / num2));
+        } else {
+            System.out.println("Division by zero is not allowed.");
+        }
+        scanner.close();
     }
+}
 
 }
