c8bd470d70803dc9344fd53d384e5f109f48b80f
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-01 17:52:47 +0000

    Sum of a sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,15 +2,17 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-public class Counting {
+
+public class SumOfSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.print("Last number? ");
         int number = scanner.nextInt();
-        
-        for (int i = 0; i <= number; i++) {
-            System.out.println(i);
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
         }
+        System.out.println("The sum is " + sum);
         scanner.close();
     }
 }
