43b0df187c4074485c3f5642ab0974e4d84dc5a6
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-02 07:48:57 +0000

    reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,40 +2,24 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-public class NumberOperations {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("How many numbers do you want to enter? ");
-        int n = scanner.nextInt();
-        
-        int[] numbers = new int[n];
-        int sum = 0;
-        int evenCount = 0;
-        int oddCount = 0;
-        
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = scanner.nextInt();
-            sum += numbers[i];
-            if (numbers[i] % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
+            System.out.println("How many times?");
+            int times = scanner.nextInt();
+
+            int count = 0;
+
+            while (count < times) {
+                printText();
+                count++;
             }
+            scanner.close();
         }
-        System.out.println("The sum of the numbers is: " + sum);
 
-        System.out.println("The sum is: " + sum);
-        System.out.println("The number of numbers is: " + n);
-
-        double average = (double) sum / n;
-        System.out.println("The average of the numbers is: " + average);
-
-        System.out.println("The number of even numbers is: " + evenCount);
-        System.out.println("The number of odd numbers is: " + oddCount);
+        public static void printText() {
+            System.out.println("In a hole in the ground there lived a method");
+        }
     }
-}
-
 }
