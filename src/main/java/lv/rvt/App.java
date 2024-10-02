a569fde45d0095d3e69edcf063b9cc9e99f50878
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-02 06:52:52 +0000

    Visi 5

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,42 +2,40 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int sum = 0;
-            int count = 0;
-            int evenCount = 0;
-            int oddCount = 0;
-            
-            System.out.println("Give numbers:");
-            
-            while (true) {
-                int number = scanner.nextInt();
-                
-                if (number == -1) {
-                    System.out.println("Thx! Bye!");
-                    break;
-                }
-                
-                sum += number;
-                count++;
-                
-                if (number % 2 == 0) {
-                    evenCount++;
-                } else {
-                    oddCount++;
-                }
+
+public class NumberOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("How many numbers do you want to enter? ");
+        int n = scanner.nextInt();
+        
+        int[] numbers = new int[n];
+        int sum = 0;
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+            if (numbers[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
             }
-            
-            double average = (double) sum / count;
-            
-            System.out.println("Sum: " + sum);
-            System.out.println("Numbers: " + count);
-            System.out.println("Average: " + average);
-            System.out.println("Even: " + evenCount);
-            System.out.println("Odd: " + oddCount);
         }
+        System.out.println("The sum of the numbers is: " + sum);
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The number of numbers is: " + n);
+
+        double average = (double) sum / n;
+        System.out.println("The average of the numbers is: " + average);
+
+        System.out.println("The number of even numbers is: " + evenCount);
+        System.out.println("The number of odd numbers is: " + oddCount);
     }
-    
+}
+
 }
