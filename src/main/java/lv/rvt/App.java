c66c5aa89729c39c7f72e34c24f16902fee62d0e
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-02 06:44:09 +0000

    sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,28 @@
 package lv.rvt;
 import java.util.Scanner;
 public class App
-{public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+{
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        
+        System.out.println("Give numbers:");
+        
+        while (true) {
+            int number = scanner.nextInt();
             
-            System.out.println("Give numbers:");
-            
-            while (true) {
-                int number = scanner.nextInt();
-                
-                if (number == -1) {
-                    System.out.println("Thx! Bye!");
-                    break;
-                }
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                break;
             }
+            
+            sum += number;
         }
+        
+        System.out.println("Sum: " + sum);
     }
+}
+
+}
