c3a46828b4dce29c5fc1ca1ab64336e9a3cdc0f3
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-09 07:03:16 +0000

    DivisableByThreeIzlabots

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,19 +2,26 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-    public class Main {
-        public static void divisibleByThreeInRange(int beginning, int end) {
-            for (int i = beginning; i <= end; i++) {
-                if (i % 3 == 0) {
-                    System.out.println(i);
-                }
+public class Main {
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
             }
         }
-    
-        public static void main(String[] args) {
-            divisibleByThreeInRange(3, 6);
-            divisibleByThreeInRange(2, 10);
-        }
     }
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ievadiet diapazona sākumu: ");
+        int beginning = scanner.nextInt();
+        
+        System.out.print("Ievadiet diapazona beigas: ");
+        int end = scanner.nextInt();
+        
+        divisibleByThreeInRange(beginning, end);
+    }
+}
+
 }
