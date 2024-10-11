b51cb90cf39eb17b4c94fd0874f82201d1036cd1
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-11 07:56:26 +0000

    Stars

---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,24 @@
+package lv.rvt;
+public class Stars {
+public static void printStars(int number) {
+    for(int i = 0; i < number; i++){
+        System.out.println("*");
+      }
+    System.out.println();
+    }
+    public static void pritSquare(int size) {
+    for(int i = 0; i < size; i++) {
+        printStars(size);
+       }
+    }
+    public static void printRectangle(int width, int height) {
+    for(int i = 0; i < height; i++){
+        printStars(width);
+       }
+    }
+    public static void printTriangle(int size){
+    for(int i = 1; i <=size; i++){
+        printStars(i);
+        }
+    }
+}
