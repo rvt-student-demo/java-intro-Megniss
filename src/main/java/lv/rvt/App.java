2e1934a1d2c4087ba5b8fe908e607e358b6cd92e
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-16 07:46:51 +0000

    Second plus Third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,33 @@
 package lv.rvt;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class App
+
 {
-    public static void main(String[] args )
-    {
+    
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Stars.printRectangle(17, 3);
-        scanner.close();
-    }
-    public static void divisibalByThreeInRange(int begining, int end){
-        for(int i = begining; i <= end; i++) {
-            if(i % 3 == 0){
-                System.out.println(i);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        System.out.println("Enter integers (0 to stop):");
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == 0) {
+                break;
             }
+            numbers.add(number);
         }
+        System.out.println("Numbers entered:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        
+        System.out.println("The sum of all elements is: " + sum);
+        
+        scanner.close();
     }
 }
