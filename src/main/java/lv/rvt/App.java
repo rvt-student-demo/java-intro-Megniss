6b06dbfab055d77f759eb4164da0d36f6d6cbf00
Megniss <166013473+Megniss@users.noreply.github.com>
2024-10-30 08:51:35 +0000

    PersolaDetails

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,29 +4,32 @@ import java.util.ArrayList;
 public class App
 
 {
-
         public static void main(String[] args) {
-            ArrayList<Integer> numbers = new ArrayList<>();
-            numbers.add(3);
-            numbers.add(2);
-            numbers.add(6);
-            numbers.add(-1);
-            numbers.add(5);
-            numbers.add(1);
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<String> names = new ArrayList<>();
+            ArrayList<Integer> birthYears = new ArrayList<>();
+            while (true) {
+                String input = scanner.nextLine();
+                if (input.isEmpty()) {
+                    break;
+                }
     
-            System.out.println("The numbers in the range [0, 5]");
-            printNumbersInRange(numbers, 0, 5);
-    
-            System.out.println("The numbers in the range [3, 10]");
-            printNumbersInRange(numbers, 3, 10);
-        }
-    
-        public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
-            for (int number : numbers) {
-                if (number >= lowerLimit && number <= upperLimit) {
-                    System.out.println(number);
+                String[] parts = input.split(",");
+                names.add(parts[0]);
+                birthYears.add(Integer.parseInt(parts[1]));
+            }
+            String longestName = "";
+            for (String name : names) {
+                if (name.length() > longestName.length()) {
+                    longestName = name;
                 }
             }
+            int sum = 0;
+            for (int year : birthYears) {
+                sum += year;
+            }
+            double average = sum / (double) birthYears.size();
+            System.out.println("Longest name: " + longestName);
+            System.out.println("Average of the birth years: " + average);
         }
-    }
-    
+}
